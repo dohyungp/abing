292b153994046c73a6b365f8444f4e6ee063977c
Dohyung Park <dohyung.prk@gmail.com>
2020-12-04 16:49:46 +0900

    experiment/arm 모델 스키마 구축

--- abitrary/backend/api/app/models/arm.py
+++ abitrary/backend/api/app/models/arm.py
@@ -1,5 +1,16 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 from app.db.base_class import Base
+
+if TYPE_CHECKING:
+    from .experiment import Experiment
 
 
 class Arm(Base):
-    pass
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(150), index=True)
+    experiment_id = Column(Integer, ForeignKey("experiment.id"))
+    experiment = relationship("Experiment", back_populates="arms")
--- abitrary/backend/api/app/models/experiment.py
+++ abitrary/backend/api/app/models/experiment.py
@@ -1,10 +1,19 @@
-from sqlalchemy import Column, Integer, String
+from typing import TYPE_CHECKING
+
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.orm import relationship
 
-from app.db.base_class import Base
+from app.db.base import Base
+
+if TYPE_CHECKING:
+    from .arm import Arm  # noqa: F401
 
 
 class Experiment(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String, index=True)
+    name = Column(String(200), index=True)
+    is_running = Column(Boolean(), default=False)
     description = Column(String, null=True)
+    start_date = Column(DateTime, null=True)
+    end_date = Column(DateTime, null=True)
+    arms = relationship("Arm", back_populates="experiment")
