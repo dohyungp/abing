89c58f162ac1985017156f5f6538e562ebda0ce3
Dohyung Park <dohyung.prk@gmail.com>
2020-12-04 19:21:24 +0900

    dict None 처리 변경

--- abitrary/backend/api/app/core/config.py
+++ abitrary/backend/api/app/core/config.py
@@ -25,7 +25,7 @@ class Settings(BaseSettings):
             password=values.get("POSTGRES_PASSWORD"),
             host=values.get("POSTGRES_SERVER"),
             port=values.get("POSTGRES_PORT"),
-            path=f"/{values.get('POSTGRES_DB') or ''}",
+            path=f"/{values.get('POSTGRES_DB', '')}",
         )
 
     class Config:
