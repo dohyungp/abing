8000b01ee1da0a58ad85f8833bb75a4e269a11bb
Dohyung Park <dohyung.prk@gmail.com>
2020-12-04 19:17:26 +0900

    setting 추가

---
+++ abitrary/backend/api/app/core/config.py
@@ -0,0 +1,35 @@
+import secrets
+from pydantic import BaseSettings, PostgresDsn, validators
+
+
+class Settings(BaseSettings):
+
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_PORT: str = "5432"
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(
+        cls, v: Optional[str], values: Dict[str, Any]
+    ) -> Any:  # noqa
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            port=values.get("POSTGRES_PORT"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+    class Config:
+        case_sensitive = False
+
+
+settings = Settings()
