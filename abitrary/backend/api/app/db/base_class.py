69490c719b36228ba7174667e2c7db2e44c93598
Dohyung Park <dohyung.prk@gmail.com>
2020-12-04 14:15:26 +0900

    decorator noqa 설정

--- abitrary/backend/api/app/db/base_class.py
+++ abitrary/backend/api/app/db/base_class.py
@@ -9,5 +9,5 @@ class Base:
     __name__: str
     # Generate __tablename__ automatically
     @declared_attr
-    def __tablename__(cls) -> str:
+    def __tablename__(cls) -> str:  # noqa
         return cls.__name__.lower()
