74f3a00880936e197b70796c2f305f9aa576c25d
Dohyung Park <dohyung.prk@gmail.com>
2020-12-20 16:34:20 +0900

    API Swagger Project 이름 추가

--- abitrary/backend/api/app/main.py
+++ abitrary/backend/api/app/main.py
@@ -7,6 +7,6 @@ from sqlalchemy.orm import Session
 from app.routes.v1.api import api_router
 from app.core.config import settings
 
-app = FastAPI()
+app = FastAPI(title=settings.PROJECT_NAME)
 
 app.include_router(api_router, prefix=settings.API_VERSION)
