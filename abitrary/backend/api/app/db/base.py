24b0deb5a93b3f5031573aa00b7c49ad71b069fe
Dohyung Park <dohyung.prk@gmail.com>
2020-12-23 12:06:42 +0900

    add user model

--- abitrary/backend/api/app/db/base.py
+++ abitrary/backend/api/app/db/base.py
@@ -1,6 +1,7 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 from app.db.base_class import Base  # noqa
+from app.models.user import User  # noqa
 from app.models.experiment import Experiment  # noqa
 from app.models.arm import Arm  # noqa
 from app.models.feature import Feature  # noqa
