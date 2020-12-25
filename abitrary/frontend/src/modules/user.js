fde59e2bb14a1fce1ef42b2fe246251cec531df2
Dohyung Park <dohyung.prk@gmail.com>
2020-12-26 02:26:58 +0900

    add userlist api

--- abitrary/frontend/src/modules/user.js
+++ abitrary/frontend/src/modules/user.js
@@ -1,26 +1,36 @@
-import * as loginAPI from "../api/login";
+import * as userAPI from "../api/user";
 import {
   reducerUtils,
   handleAsyncActions,
   createPromiseSaga,
 } from "../libs/asyncUtil";
-import { takeLatest } from "redux-saga/effects";
+import { takeEvery, takeLatest } from "redux-saga/effects";
 
 /* 액션 타입 */
 const LOGIN_REQUEST = "LOGIN_REQUEST";
 const LOGIN_SUCCESS = "LOGIN_REQUEST_SUCCESS";
 const LOGIN_ERROR = "LOGIN_REQUEST_ERROR";
+const GET_USERS = "GET_USERS";
+const GET_USERS_SUCCESS = "GET_USERS_SUCCESS";
+const GET_USERS_ERROR = "GET_USERS_ERROR";
 
 export const loginRequest = (data) => ({ type: LOGIN_REQUEST, payload: data });
+export const getUsers = (data) => ({ type: GET_USERS, payload: data });
 
-const requestLoginSaga = createPromiseSaga(LOGIN_REQUEST, loginAPI.login);
+const requestLoginSaga = createPromiseSaga(LOGIN_REQUEST, userAPI.login);
+const getUserListSaga = createPromiseSaga(GET_USERS, userAPI.getUsers);
 
 export function* loginSaga() {
   yield takeLatest(LOGIN_REQUEST, requestLoginSaga);
 }
 
+export function* userListSaga() {
+  yield takeEvery(GET_USERS, getUserListSaga);
+}
+
 const initialState = {
   login: reducerUtils.initial(),
+  users: reducerUtils.initial(),
 };
 
 export default function login(state = initialState, action) {
@@ -29,6 +39,10 @@ export default function login(state = initialState, action) {
     case LOGIN_SUCCESS:
     case LOGIN_ERROR:
       return handleAsyncActions(LOGIN_REQUEST, "login", true)(state, action);
+    case GET_USERS:
+    case GET_USERS_SUCCESS:
+    case GET_USERS_ERROR:
+      return handleAsyncActions(GET_USERS, "users", true)(state, action);
     default:
       return state;
   }
--- abitrary/frontend/src/pages/HomePage.jsx
+++ abitrary/frontend/src/pages/HomePage.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import AuthContainer from "../containers/AuthContainer";
+import { getUsers } from "../modules/user";
 
 const HomePage = () => {
-  return <AuthContainer>홈페이지</AuthContainer>;
+  const { login } = useSelector((state) => state.login);
+  const { users } = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(
+      getUsers({
+        access_token: login.data?.access_token,
+      }),
+    );
+  }, [dispatch, login]);
+  return <AuthContainer>{JSON.stringify(users)}</AuthContainer>;
 };
 
 export default HomePage;
