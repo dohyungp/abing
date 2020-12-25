26b147e149c4fb271e94a48af93bb89f5de6b221
Dohyung Park <dohyung.prk@gmail.com>
2020-12-25 14:38:20 +0900

    remove counter testcode

--- abitrary/frontend/src/App.jsx
+++ abitrary/frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import "./App.css";
-import CounterContainer from "./containers/CounterContainer";
 import LoginFormContainer from "./containers/LoginContainer";
 
 const App = () => (
   <div className="App">
-    <CounterContainer />
     <LoginFormContainer />
   </div>
 );
