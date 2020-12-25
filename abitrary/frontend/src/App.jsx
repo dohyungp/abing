212528c6c35b9f71257e118d14079a2f74b09833
Dohyung Park <dohyung.prk@gmail.com>
2020-12-25 22:16:56 +0900

    add home

--- abitrary/frontend/src/App.jsx
+++ abitrary/frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import "./App.css";
 
 const App = () => (
   <>
+    <Route path="/" component={HomePage} exact={true} />
     <Route path="/login" component={LoginPage} exact={true} />
   </>
 );
