fb05e2a8df5a791147ec2545727801e34ee7f56f
Dohyung Park <dohyung.prk@gmail.com>
2020-12-25 15:18:02 +0900

    remove unused script

--- abitrary/frontend/src/index.js
+++ abitrary/frontend/src/index.js
@@ -10,7 +10,6 @@ import { Router } from "react-router-dom";
 import createSagaMiddleware from "redux-saga";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
 
 axios.defaults.baseURL =
@@ -40,8 +39,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root"),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
