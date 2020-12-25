1dd0b2b55c7c4765527a045ff57c0f602a912010
Dohyung Park <dohyung.prk@gmail.com>
2020-12-25 13:53:51 +0900

    move to center a login form

--- abitrary/frontend/src/containers/LoginContainer.jsx
+++ abitrary/frontend/src/containers/LoginContainer.jsx
@@ -1,11 +1,18 @@
 import LoginForm from "../components/LoginForm";
+import logo from "./abitrary-logo.png";
+import "./loginformcontainer.css";
 
 const LoginFormContainer = () => {
   const onFinish = (values) => {
     console.log("Received values of form: ", values);
   };
 
-  return <LoginForm onFinish={onFinish} />;
+  return (
+    <div className="login-form-container">
+      <img src={logo} alt="abitrary" className="login-logo" />
+      <LoginForm onFinish={onFinish} />
+    </div>
+  );
 };
 
 export default LoginFormContainer;
