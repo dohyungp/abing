972e436b821b64ab2719de79f5d7dd8482a34b77
Dohyung Park <dohyung.prk@gmail.com>
2020-12-26 21:49:42 +0900

    add create user feature

--- abitrary/frontend/src/containers/UserTableContainer.jsx
+++ abitrary/frontend/src/containers/UserTableContainer.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
+import { Button, Col, Row } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import UserTable from "../components/UserTable";
-import { getUsers } from "../actions/users/users";
+import { createUser, getUsers } from "../actions/users/users";
 
 const UserTableContainer = () => {
   const { login } = useSelector((state) => state.login);
@@ -14,7 +15,37 @@ const UserTableContainer = () => {
       }),
     );
   }, [dispatch, login]);
-  return <UserTable data={users.data} loading={users.loading} />;
+
+  const onButtonClick = (e) => {
+    e.preventDefault();
+    dispatch(
+      createUser({
+        access_token: login.data?.access_token,
+        email: "test4@test.com",
+        name: "string",
+        password: "string",
+      }),
+    );
+  };
+  return (
+    <>
+      <Row justify="end">
+        <Button
+          onClick={onButtonClick}
+          style={{ margin: 16 }}
+          type="primary"
+          ghost
+        >
+          유저 추가
+        </Button>
+      </Row>
+      <Row justify="center">
+        <Col span={24}>
+          <UserTable data={users.data} loading={users.loading} />
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default UserTableContainer;
--- abitrary/frontend/src/reducers/users.js
+++ abitrary/frontend/src/reducers/users.js
@@ -2,7 +2,8 @@ import * as types from "../actions/users/users";
 import { reducerUtils, handleAsyncActions } from "../libs/asyncUtil";
 
 const initialState = {
-  users: reducerUtils.initial(),
+  users: reducerUtils.initial([]),
+  user: reducerUtils.initial(),
 };
 
 export default function users(state = initialState, action) {
@@ -11,6 +12,10 @@ export default function users(state = initialState, action) {
     case types.GET_USERS_SUCCESS:
     case types.GET_USERS_ERROR:
       return handleAsyncActions(types.GET_USERS, "users", true)(state, action);
+    case types.CREATE_USER:
+    case types.CREATE_USER_SUCCESS:
+    case types.CREATE_USER_ERROR:
+      return handleAsyncActions(types.CREATE_USER, "user", true)(state, action);
     default:
       return state;
   }
