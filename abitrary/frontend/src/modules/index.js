15816e85b9e451828c5d30eb18ba75ccd6d94761
Dohyung Park <dohyung.prk@gmail.com>
2020-12-26 13:16:47 +0900

    remove counter saga

--- abitrary/frontend/src/modules/index.js
+++ abitrary/frontend/src/modules/index.js
@@ -1,12 +1,11 @@
 import { combineReducers } from "redux";
 import { all } from "redux-saga/effects";
-import counter, { counterSaga } from "./counter";
 import login, { loginSaga } from "./user";
 import users, { userListSaga } from "./user";
 
-const rootReducer = combineReducers({ counter, login, users });
+const rootReducer = combineReducers({ login, users });
 export function* rootSaga() {
-  yield all([counterSaga(), loginSaga(), userListSaga()]);
+  yield all([loginSaga(), userListSaga()]);
 }
 
 export default rootReducer;
