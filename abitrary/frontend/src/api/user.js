b07214a9e1f44acc7ac89eb3e387ba5bf4cd4938
Dohyung Park <dohyung.prk@gmail.com>
2020-12-27 15:56:04 +0900

    move login api function to auth.js

--- abitrary/frontend/src/api/user.js
+++ abitrary/frontend/src/api/user.js
@@ -1,13 +1,5 @@
 import axios from "axios";
 
-export const login = async ({ username, password }) => {
-  let form = new FormData();
-  form.append("username", username);
-  form.append("password", password);
-  const response = await axios.post("/api/v1/login/", form);
-  return response.data;
-};
-
 export const getUsers = async ({ access_token, skip = 0, limit = 100 }) => {
   const response = await axios.get("/api/v1/users/", {
     params: { skip, limit },
--- abitrary/frontend/src/sagas/auth.js
+++ abitrary/frontend/src/sagas/auth.js
@@ -1,9 +1,9 @@
 import { LOGIN_REQUEST } from "../actions/users/auth";
 import { createPromiseSaga } from "../libs/asyncUtil";
-import * as userAPI from "../api/user";
+import * as authAPI from "../api/auth";
 import { takeLatest } from "redux-saga/effects";
 
-const loginSaga = createPromiseSaga(LOGIN_REQUEST, userAPI.login);
+const loginSaga = createPromiseSaga(LOGIN_REQUEST, authAPI.login);
 
 export default function* watchLogin() {
   yield takeLatest(LOGIN_REQUEST, loginSaga);
