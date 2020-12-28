447b88770ffb7644bcb6d2c4c7948f9f1b010881
Dohyung Park <dohyung.prk@gmail.com>
2020-12-28 12:17:17 +0900

    :bug: change access data path

--- abitrary/frontend/src/reducers/auth.js
+++ abitrary/frontend/src/reducers/auth.js
@@ -4,7 +4,7 @@ import { reducerUtils } from "../libs/asyncUtil";
 export default function auth(state = reducerUtils.initial(), action) {
   switch (action.type) {
     case types.LOGIN_REQUEST:
-      return reducerUtils.loading(state.auth?.data);
+      return reducerUtils.loading(state.data);
     case types.LOGIN_SUCCESS:
       return reducerUtils.success(action.payload);
     case types.LOGIN_ERROR:
