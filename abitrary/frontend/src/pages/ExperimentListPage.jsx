5bf19c41635e811ff7c3d28b748bb57443d8f7c6
Dohyung Park <dohyung.prk@gmail.com>
2020-12-28 12:52:57 +0900

    add experiment related pages

---
+++ abitrary/frontend/src/pages/ExperimentListPage.jsx
@@ -0,0 +1,5 @@
+const ExperimentListPage = () => {
+  return <div></div>;
+};
+
+export default ExperimentListPage;
---
+++ abitrary/frontend/src/pages/ExperimentPage.jsx
@@ -0,0 +1,5 @@
+const ExperimentPage = () => {
+  return <div></div>;
+};
+
+export default ExperimentPage;
