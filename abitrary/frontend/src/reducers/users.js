18f3ed8c653f2acee95db74ecb25940e4a05f954
Dohyung Park <dohyung.prk@gmail.com>
2020-12-28 12:04:25 +0900

    :bug: change state.users.data --> state.data

--- abitrary/frontend/src/reducers/users.js
+++ abitrary/frontend/src/reducers/users.js
@@ -4,17 +4,17 @@ import { reducerUtils } from "../libs/asyncUtil";
 export default function users(state = reducerUtils.initial([]), action) {
   switch (action.type) {
     case types.GET_USERS:
-      return reducerUtils.loading(state.users?.data);
+      return reducerUtils.loading(state.data);
     case types.GET_USERS_SUCCESS:
       return reducerUtils.success(action.payload);
     case types.GET_USERS_ERROR:
       return reducerUtils.error(action.payload);
     case types.CREATE_USER:
-      return reducerUtils.loading(state.users?.data);
+      return reducerUtils.loading(state.data);
     case types.CREATE_USER_SUCCESS:
-      return reducerUtils.success([...state.users.data, action.payload]);
+      return reducerUtils.success([...state.data, action.payload]);
     case types.CREATE_USER_ERROR:
-      return reducerUtils.error(action.payload, state.users.data);
+      return reducerUtils.error(action.payload, state.data);
     default:
       return state;
   }
