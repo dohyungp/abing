7338572fa617a9d8afd407198e1cb12ea2c02a8c
Dohyung Park <dohyung.prk@gmail.com>
2020-12-28 16:11:54 +0900

    :sparkles: add mobile version menu

--- abitrary/frontend/src/components/global/Header.jsx
+++ abitrary/frontend/src/components/global/Header.jsx
@@ -1,16 +1,47 @@
-import { Col, Menu, Row } from "antd";
+import { Button, Col, Drawer, Menu, Row } from "antd";
 import { MenuOutlined } from "@ant-design/icons";
 import { NavLink, useLocation } from "react-router-dom";
 import logo from "../../assets/abitrary-logo.png";
 import "./header.css";
+import { useState } from "react";
 
 const Header = () => {
   const location = useLocation();
+  const [drawerVisible, setDrawerVisble] = useState(false);
   return (
     <header className="abitrary-header">
-      <span className="phone-menu-icon">
-        <MenuOutlined />
+      <span className="mobile-menu-icon">
+        <Button
+          type="text"
+          style={{ padding: "0", height: "0" }}
+          onClick={() => setDrawerVisble(true)}
+        >
+          <MenuOutlined />
+        </Button>
       </span>
+      <Drawer
+        title="ABITRARY"
+        key="right"
+        visible={drawerVisible}
+        closable={false}
+        bodyStyle={{ padding: "0" }}
+        onClose={() => setDrawerVisble(false)}
+      >
+        <Menu
+          mode="vertical-right"
+          className="abitrary-mobile-menu"
+          defaultSelectedKeys={["/"]}
+          selectedKeys={[location.pathname]}
+        >
+          <Menu.Item className="menu-item" key="/">
+            <NavLink to="/">Home</NavLink>
+          </Menu.Item>
+          <Menu.Item className="menu-item" key="/users">
+            <NavLink to="/users">Users</NavLink>
+          </Menu.Item>
+          <Menu.Item className="menu-item">Experiments</Menu.Item>
+        </Menu>
+      </Drawer>
       <Row>
         <Col xs={24} sm={24} md={4} lg={6} xl={8}>
           <img src={logo} alt="ABITRARY" className="logo" />
