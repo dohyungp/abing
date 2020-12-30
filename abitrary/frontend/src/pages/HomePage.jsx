ba9d9a79fc4642823e6fcd48e2117abbc6c7611c
Dohyung Park <dohyung.prk@gmail.com>
2020-12-31 01:53:30 +0900

    add loginRequired option to home page

--- abitrary/frontend/src/pages/HomePage.jsx
+++ abitrary/frontend/src/pages/HomePage.jsx
@@ -1,6 +1,12 @@
 import Layout from "../components/global/Layout";
+import LoginRequred from "../containers/global/LoginRequred";
 const HomePage = () => {
-  return <Layout>홈페이지</Layout>;
+  return (
+    <Layout>
+      <LoginRequred />
+      홈페이지
+    </Layout>
+  );
 };
 
 export default HomePage;
