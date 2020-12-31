5200c242f07692c459ad2303f807be1edfde53d8
Dohyung Park <dohyung.prk@gmail.com>
2021-01-01 00:22:06 +0900

    add 404 style

--- abitrary/frontend/src/pages/404Page.jsx
+++ abitrary/frontend/src/pages/404Page.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Result, Button } from "antd";
+import { Link } from "react-router-dom";
 
 const Page404 = () => {
-  return <div>Not found</div>;
+  return (
+    <div>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Link to="/">
+            <Button type="primary">Back Home</Button>
+          </Link>
+        }
+      />
+    </div>
+  );
 };
 
 export default Page404;
