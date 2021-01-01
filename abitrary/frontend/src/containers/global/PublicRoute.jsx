70e0395cc54cdff9dc48fd845fc6db13df824ef0
Dohyung Park <dohyung.prk@gmail.com>
2021-01-02 01:02:22 +0900

    :bug: remove getMe dispatch from public route

--- abitrary/frontend/src/containers/global/PublicRoute.jsx
+++ abitrary/frontend/src/containers/global/PublicRoute.jsx
@@ -1,14 +1,8 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
-import { getMe } from "../../actions/users/me";
 
 const PrivateRoute = ({ comp: Component, ...rest }) => {
   const token = useSelector((state) => state.auth?.data?.access_token);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getMe());
-  }, [dispatch]);
   return (
     <Route
       {...rest}
