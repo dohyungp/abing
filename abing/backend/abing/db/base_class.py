be6792320947c26dfb94b6a1da825b9de00e909b
Dohyung Park <dohyung.prk@gmail.com>
2021-01-19 15:55:11 +0900

    :shirt: lint disable 추가

--- abing/backend/abing/db/base_class.py
+++ abing/backend/abing/db/base_class.py
@@ -1,3 +1,4 @@
+"""Base class"""
 from typing import Any
 
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
@@ -5,9 +6,11 @@ from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 @as_declarative()
 class Base:
+    """DB Base class"""
+
     id: Any
     __name__: str
     # Generate __tablename__ automatically
     @declared_attr
-    def __tablename__(cls) -> str:  # noqa
+    def __tablename__(cls) -> str:  # pylint: disable=no-self-argument
         return cls.__name__.lower()
